31c141bb471daef13690b55f8630f7fc56973cd7
Md Najim Ahmed <najim.ju@gmail.com>
2018-07-20 21:37:03 +0600

    init

---
+++ src/main/java/com/example/cefaloschoolproject/CefaloSchoolProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.cefaloschoolproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CefaloSchoolProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CefaloSchoolProjectApplication.class, args);
+	}
+}
